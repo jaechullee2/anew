8318baeab3b92853a52dc12adbc3a22929742212
Jayden <chel8780@gmail.com>
2022-10-24 18:01:02 +0900

    Commit 10/24
    10월 24일 할당

--- shells/case1.sh
+++ shells/case1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-case "$1" in
-	ko)
-		echo "Seoul" ;;
-	us)
-		echo "Washington" ;;
-	cn)
-		echo "Beijing" ;;
-	jp)
-		echo "Tokyo" ;;
-	*)
-		echo "Enter the contry name~!!"
+if [ $# -eq 0 ]; then
+	echo "Enter the contry name~!!"
+else 
+	case "$1" in
+		ko)
+			echo "Seoul" ;;
+		us)
+			echo "Washington" ;;
+		cn)
+			echo "Beijing" ;;
+		jp)
+			echo "Tokyo" ;;
+		*)
+			echo "Your entry => $1 your entry is not in the list"
 esac
+fi
